d3bb2a57ba99d7c894e6e11b82f8bd385b00b167
Dubrovskiy Anatoliy <dubrovskiy.ru@mail.ru>
2022-09-28 07:22:02 +0300

    create test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import { ProductsService } from './services/products.service';
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-project';
+export class AppComponent implements OnInit {
+  title = 'angular project'
+  loading = false
+  products$: Observable<IProduct[]> // стрим
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+  }
 }
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { IProduct } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() product: IProduct
+
+  details = false
+
+  ngOnInit(): void {
+  }
+
+}
